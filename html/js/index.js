94f4d6e2838b80a246966fd4a5462162a023df80
Pedrobsperes <165672963+Pedrobsperes@users.noreply.github.com>
2024-05-27 11:50:15 -0300

    Add files via upload

---
+++ html/js/index.js
@@ -0,0 +1,35 @@
+//mostrar na lela
+/*window.alert("Bem vindo ao javascript linkado."); 
+confirm("deseja continuar?");
+var Nome;
+Nome = prompt ("Digite aqui seu nome:"); //abre uma caixa de texto
+document.write ('Oi ' + Nome + '    seja bem vindo(a)!'); // ecreve na sua pagina*/
+
+/*var N1;
+var N2;
+var R;
+
+N1 = prompt('Digite o 1 valor');
+N2 = prompt('Digite o 2 valor');
+
+R = parseInt (N1) + parseInt (N2);      //calcular as variaveis inteiras 
+
+document.write ('Resultado =' + R);*/
+
+
+    // calcular a media
+var NOTA1; 
+var NOTA2;
+var MEDIA;
+
+NOTA1 = prompt('Digite a 1 nota', 'digite aqui:');
+NOTA2 = prompt('Digite a 2 nota', 'Digite aqui:');
+
+MEDIA = (parseFloat) (NOTA1) + parseFloat (NOTA2) /2;
+var nome;
+document.write("Resultado =" + MEDIA);
+console.log("mensagem enviada para o console");
+nome = prompt("digite seu nome");
+console.log(nome.length);
+
+
